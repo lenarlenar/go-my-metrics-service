7c0a8acc4c83fb797ba457368ce1d5bf0083b9bb
lenarlenar <returnundefile@gmail.com>
2025-01-02 22:55:25 +0300

    Code for iter3.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,78 +1,67 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lenarlenar/go-my-metrics-service/internal/db"
 )
 
-type MemStorage struct {
-	Gauge map[string]float64
-	Counter map[string]int64
-}
+var memStorage db.MemStorage
 
-type MetricsDB interface {
-	SetGauge(n string, v float64)
-	AddCounter(n string, v int64)
-}
-
-func (m *MemStorage) SetGauge(n string, v float64) {
-	m.Gauge[n] = v
-}
-
-func (m *MemStorage) AddCounter(n string, v int64) {
-	val, ok := m.Counter[n]
-	if ok {
-		m.Counter[n] = val + v
-	} else {
-		m.Counter[n] =  v
-	}
-}
-
-var memStorage MemStorage
 func main() {
-	memStorage = MemStorage {map[string]float64{}, map[string]int64{}}
-	if err := run(); err != nil {
-		panic(err)
-	}
+	memStorage = db.MemStorage{Gauge: map[string]float64{}, Counter: map[string]int64{}}
+	router := gin.Default()
+	router.POST("/update/:type/:name/:value", updateHandler)
+	router.GET("/value/:type/:name/", valueHandler)
+	router.Run()
 }
 
-func run() error {
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/update/`, defaultHandler)
-	return http.ListenAndServe(":8080", mux)
-}
-
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	parts := strings.Split(r.URL.Path, "/")
-
-	if len(parts) != 5 {
-		http.Error(w, "Invalid URL structure", http.StatusNotFound)
-		return
-	}
-
-	metricType := parts[2]
-	metricName := parts[3]
+func valueHandler(c *gin.Context) {
+	metricType := c.Param("type")
+	metricName := c.Param("name")
 
 	switch metricType {
 	case "gauge":
-		if metricValue, err := strconv.ParseFloat(parts[4], 64); err != nil {
-			http.Error(w, "Value must be float64", http.StatusBadRequest)
+		if val, ok := memStorage.Gauge[metricName]; ok {
+			c.String(http.StatusOK, fmt.Sprintf("%g", val))
+		} else {
+			c.String(http.StatusNotFound, "Unknown metric name")
+		}
+	case "counter":
+		if val, ok := memStorage.Counter[metricName]; ok {
+			c.String(http.StatusOK, fmt.Sprintf("%d", val))
+		} else {
+			c.String(http.StatusNotFound, "Unknown metric name")	
+		}
+	default:
+		c.String(http.StatusNotFound, "Unknown metric type")
+	}
+}
+
+func updateHandler(c *gin.Context) {
+	metricType := c.Param("type")
+	metricName := c.Param("name")
+	metricValue := c.Param("value")
+
+	switch metricType {
+	case "gauge":
+		if metricValue, err := strconv.ParseFloat(metricValue, 64); err != nil {
+			c.String(http.StatusBadRequest, "Value must be float64")
 		} else {
 			memStorage.SetGauge(metricName, metricValue)
 		}
 	case "counter":
-		if metricValue, err := strconv.ParseInt(parts[4], 0, 64); err != nil {
-			http.Error(w, "Value must be int64", http.StatusBadRequest)
+		if metricValue, err := strconv.ParseInt(metricValue, 0, 64); err != nil {
+			c.String(http.StatusBadRequest, "Value must be int64")
 		} else {
 			memStorage.AddCounter(metricName, metricValue)
 		}
 	default:
-		http.Error(w, "Unknown metric name", http.StatusBadRequest)
+		c.String(http.StatusBadRequest, "Unknown metric name")
 	}
+
+	c.String(http.StatusOK, "Запрос успешно обработан")
 }
