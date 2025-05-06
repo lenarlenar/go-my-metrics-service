cdf2369d7660a7099eb9891dfb2f7b35e0ff8e94
lenarlenar <returnundefine@gmail.com>
2025-05-06 09:00:52 +0300

    Refactoring: move workerpool.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,16 +4,15 @@ import (
 	"context"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
 
 	"github.com/lenarlenar/go-my-metrics-service/internal/agent/flags"
 	"github.com/lenarlenar/go-my-metrics-service/internal/collector"
 	"github.com/lenarlenar/go-my-metrics-service/internal/log"
-	"github.com/lenarlenar/go-my-metrics-service/internal/model"
 	"github.com/lenarlenar/go-my-metrics-service/internal/sender"
 	"github.com/lenarlenar/go-my-metrics-service/internal/storage"
+	"github.com/lenarlenar/go-my-metrics-service/internal/workerpool"
 )
 
 func main() {
@@ -52,27 +51,23 @@ func main() {
 			}
 		}()
 
-		mainChan := make(chan map[string]model.Metrics, flags.RateLimit)
 		tickerReport := time.NewTicker(flags.ReportInterval)
 		defer tickerReport.Stop()
-		var wg sync.WaitGroup
 
-		log.I().Info("Запускаем воркер...")
-		for i := 0; i < flags.RateLimit; i++ {
-			wg.Add(1)
-			go worker(flags, mainChan, &wg)
-		}
+		pool := workerpool.New(flags, flags.RateLimit)
+		defer pool.Shutdown()
+
 		log.I().Infof("Запушено воркеров: %d\n", flags.RateLimit)
 
 		go func() {
 			for {
 				select {
 				case <-tickerReport.C:
-					select {
-					case mainChan <- storage.GetMetrics():
-						log.I().Info("Метрики успешно отправлены в канал")
-					default:
-						log.I().Warn("Канал переполнен, метрики не отправлены")
+					metrics := storage.GetMetrics()
+					if ok := pool.Submit(metrics); ok {
+						log.I().Info("Метрики успешно отправлены в пул")
+					} else {
+						log.I().Warn("Пул переполнен, метрики не отправлены")
 					}
 				case <-ctx.Done():
 					return
@@ -83,15 +78,6 @@ func main() {
 		<- sigs
 		log.I().Info("Получен сигнал завершения, останавливаемся...")
 		cancel()
-		close(mainChan)
-	 	wg.Wait()
 		log.I().Info("Агент завершил работу")
-	}
-}
-
-func worker(flags flags.Flags, channel chan map[string]model.Metrics, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for data := range channel {
-		sender.Send(flags, data)
 	}
 }
